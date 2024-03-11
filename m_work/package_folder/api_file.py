d10438c6349ee0f9defdd4963f4721269932242e
Majd It <majditani@gmail.com>
2024-03-11 18:50:46 +0100

    changes

--- m_work/package_folder/api_file.py
+++ m_work/package_folder/api_file.py
@@ -1,23 +1,24 @@
 from fastapi import FastAPI
 import pickle
+from package_folder.utils import from_number_to_flower
 
 app = FastAPI()
 
 @app.get("/")
-
 def root():
-    return {'Greeting ':"Hello"}
-
+    return {'greeting':"hello"}
 
 @app.get("/predict")
+def predict(sepal_length,
+            sepal_width,
+            petal_length,
+            petal_width):
 
-def predict(x1,
-            x2,
-            x3,
-            x4):
-    # remember to put models directory in root directly!!!
-    with open('models/best_model.pkl','rb') as file:
+    with open('models/best_model.pkl', 'rb') as file:
         model = pickle.load(file)
 
-    prediction = model.predict([[1,2,3,4]])
-    return {"prediction": float(prediction[0])}
+    prediction = model.predict([[sepal_length,sepal_width,petal_length,petal_width]])
+
+    pretty_prediction = from_number_to_flower(float(prediction[0]))
+
+    return {"prediction": pretty_prediction}
